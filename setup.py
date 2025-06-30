7da753caaf36a9ead1b9560fc5f6ea00c7ae0fc2
imbatony <esfak47@qq.com>
2025-06-30 11:11:24 +0800

    chore: bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pavone",
-    version="0.1.0",
+    version="0.1.1",
     author="PAVOne Team",
     description="一个集下载,整理等多功能的插件化的AV管理工具",
     long_description=long_description,
